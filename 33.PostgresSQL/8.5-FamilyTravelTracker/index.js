45e0f1eb2a6f6cd9d24645531fc6c29929d1bf8e
Micaela Jimenez <micajimenez2000@gmail.com>
2024-09-08 21:25:37 -0500

    change url

--- 33.PostgresSQL/8.5-FamilyTravelTracker/index.js
+++ 33.PostgresSQL/8.5-FamilyTravelTracker/index.js
@@ -1,16 +1,21 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import pg from 'pg';
+import env from 'dotenv';
 
 const app = express();
 const port = 3000;
 
 const db = new pg.Client({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'world',
-  password: 'Q1W2E3R44r3e2w1q',
-  port: 5432,
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+  ssl:
+    process.env.NODE_ENV === 'production'
+      ? { rejectUnauthorized: false }
+      : false,
 });
 db.connect();
 
